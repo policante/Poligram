852949d38ed1d180bf88d3d432ff34232e38b7d6
Rodrigo Martins <policante.martins@gmail.com>
2016-01-25 20:33:34 -0200

    Initial Commit

---
+++ Poligram/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Poligram
+//
+//  Created by Rodrigo Martins on 1/25/16.
+//  Copyright © 2016 Martins. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
